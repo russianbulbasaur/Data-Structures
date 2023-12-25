3f73961d19499006f1ddaa50bb3abcdf90ffc3c4
Ankit <ankit.thakur@developmentlogics.com>
2023-12-25 12:00:57 -0500

    Linear queue implementation

--- C/queueArray.c
+++ C/queueArray.c
@@ -1,5 +1,8 @@
+//Linear queue suffers with reusability problems
+//The front will keep moving forward leaving the beginning spaces unusable
+//If we move the elements in the queue then we'll have the extra overhead of moving items
 #include <stdio.h>
-#include <stblib.h>
+#include <stdlib.h>
 struct Queue{
   int capacity; //queue capacity
   int front; //to track head of queue (Consume from)
@@ -9,9 +12,21 @@ struct Queue{
 
 
 void enqueue(int value){
+     if(queue.back==queue.capacity-1){
+       printf("Queue full\n");
+       return;
+     }
+     queue.elements[++queue.back] = value;
+     printf("%i enqueued\n",value);
 }
 
-int dequeue(){
+void dequeue(){
+    if(queue.front>queue.back){
+      printf("Queue empty\n");
+      return;
+    }
+    printf("Here is %i\n",queue.elements[queue.front++]);
+    return;
 }
 
 
@@ -21,9 +36,15 @@ void main(int argc,char* argv[]){
       exit(0);
     }
     queue.front = 0;
-    queue.back = 0;
+    queue.back = -1;
     sscanf(argv[1],"%i",&queue.capacity);
     int elements[queue.capacity];
-    queue.elements = &elements;
+    queue.elements = elements;
+    enqueue(1);
+    enqueue(2);
+    enqueue(3);
+    dequeue();
+    dequeue();
+    dequeue();
 }
 
