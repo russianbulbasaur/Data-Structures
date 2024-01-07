2a407a3fa29957cef631957ac5f37385ed22e517
Ankit <ankit.thakur@developmentlogics.com>
2024-01-07 14:15:56 -0500

    Added easier implementation for circular queue

--- C/circularQueueArray.c
+++ C/circularQueueArray.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+ #include <stdlib.h>
 
 struct CircularQueue{
      int capacity;
@@ -11,26 +11,37 @@ struct CircularQueue{
 
 
 void enqueue(int value){
-     int isFull = ((queue.rear+2)%queue.capacity);
-     if(isFull){
+     if(queue.front==-1 && queue.rear==-1){
+        queue.elements[0] = value;
+        queue.front = 0;
+        queue.rear = 0;
+        printf("%i enqueued\n",queue.elements[0]);
+        return;
+     }
+     int rear = (queue.rear+1)%queue.capacity;
+     if(rear == queue.front){
        printf("The queue is full\n");
        return;
      }
-     int rear = (queue.rear+1)%queue.capacity;
      queue.rear = rear;
-     elements[rear] = value;
-     printf("%i enqueued\n",value);
+     queue.elements[rear] = value;
+     printf("%i enqueued\n",queue.elements[rear]);
 }
 
+
+
 void dequeue(){
-     int isEmpty = (((queue.rear + queue.capacity)+queue.front-1)==0);
-     if(isEmpty){
+     if(queue.front==-1){
        printf("The queue is empty\n");
        return;
      }
-     printf("Here is %i\n",elements[queue.front]);
+     printf("Here is %i\n",queue.elements[queue.front]);
      int front = (queue.front+1)%queue.capacity;
-     queue.front = front;
+     if(queue.front==queue.rear){
+        queue.front = queue.rear = -1;
+     }else{
+        queue.front = front;
+     }
 }
 
 
@@ -40,9 +51,20 @@ void main(int argc,char* argv[]){
         printf("Specify capacity\n");
         return;
      }
-     queue.rear = 0;
-     queue.front = 1;
+     queue.rear = -1;
+     queue.front = -1;
      sscanf(argv[1],"%i",&queue.capacity);
      int elements[queue.capacity];
      queue.elements = elements;
+     enqueue(1);
+     enqueue(2);
+     enqueue(3);
+     enqueue(4);
+     enqueue(5);
+     enqueue(6);
+     dequeue();
+     enqueue(6);
+     dequeue();
+     enqueue(7);
+     enqueue(8);
 }
